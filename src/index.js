29ddaa1cfd219c3dfc6565860deef642a1ef8d2e
Alice-Ermakova <alesya.ermakova91@gmail.com>
2019-10-12 08:17:35 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
 module.exports = function getTimeForEducation(
     focus = 'family', 
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
-  };
+      
+        //create variables
+	      let MasterJS_hours = 800;
+	      let BasicsProgramming_hours = 500;
+	      let Total_amount_of_hours = 0;
+          if(knowsProgramming){
+              Total_amount_of_hours = 800;
+          }
+          else{
+              Total_amount_of_hours = MasterJS_hours + BasicsProgramming_hours;
+          }
+        return result = Math.ceil(Total_amount_of_hours/config[focus]);      
+      };
   
